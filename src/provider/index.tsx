bf131ecddaea3fce607764561a6f4129c7a03413
Kevin Patinaud <you@example.com>
2023-02-01 18:49:16 +0100

    feat save account in session

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -2,15 +2,12 @@ import { FC } from "react";
 import AccountProvider from "./AccountProvider";
 import ErrorBoundary from "./ErrorBoundary";
 import IntlProvider from "./IntlProvider";
-import RouterProvider from "./Router";
 
 const AppProviders: FC<{ children: JSX.Element }> = ({ children }) => {
   return (
     <ErrorBoundary fallback={<></>}>
       <AccountProvider>
-        <RouterProvider>
-          <IntlProvider>{children}</IntlProvider>
-        </RouterProvider>
+        <IntlProvider>{children}</IntlProvider>
       </AccountProvider>
     </ErrorBoundary>
   );
